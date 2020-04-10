e357a9f1a5061abf29598316c9da6103d9c4e8d6
spencer.dezart-smith <spencer.dezart-smith@autotrader.com.au>
2020-04-10 19:47:44 +1000

    [master] Add styling to app

--- lib/src/widgets/image_list.dart
+++ lib/src/widgets/image_list.dart
@@ -7,8 +7,26 @@ class ImageList extends StatelessWidget {
 
   Widget build(context) {
     return ListView.builder(
-      itemCount: images.length,
-      itemBuilder: (context, int index) {},
+        itemCount: images.length,
+        itemBuilder: (context, int index) {
+          return buildImage(images[index]);
+        });
+  }
+
+  Widget buildImage(ImageModel image) {
+    return Container(
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.grey),
+      ),
+      padding: EdgeInsets.all(20.0),
+      margin: EdgeInsets.all(20.0),
+      child: Column(children: <Widget>[
+        Padding(
+          child: Image.network(image.url),
+          padding: EdgeInsets.only(bottom: 10.0),
+        ),
+        Text(image.title),
+      ]),
     );
   }
 }
